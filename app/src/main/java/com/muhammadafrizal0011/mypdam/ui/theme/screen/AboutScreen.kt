aaef25d1bab7c033d9faa2ae5c2ae5b1b112a482
Muhammad Afrizal <afrizalmuhammad656@gmail.com>
2025-04-11 17:29:25 +0700

    Membuat navigasi dengan tombol Up

--- app/src/main/java/com/muhammadafrizal0011/mypdam/ui/theme/screen/AboutScreen.kt
+++ app/src/main/java/com/muhammadafrizal0011/mypdam/ui/theme/screen/AboutScreen.kt
@@ -8,7 +8,11 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
@@ -24,15 +28,28 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
 import com.muhammadafrizal0011.mypdam.R
 import com.muhammadafrizal0011.mypdam.ui.theme.MyPDAMTheme
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun AboutScreen() {
+fun AboutScreen(navController: NavHostController) {
     Scaffold(
         topBar = {
             TopAppBar(
+                navigationIcon = {
+                    IconButton(onClick = {
+                        navController.popBackStack()
+                    }) {
+                        Icon(
+                            imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                            contentDescription = stringResource(R.string.back),
+                            tint = Color.White
+                        )
+                    }
+                },
                 title = { Text(text = stringResource(id = R.string.about)) },
                 colors = TopAppBarDefaults.mediumTopAppBarColors(
                     containerColor = Color.Blue,
@@ -76,6 +93,6 @@ fun AboutScreen() {
 @Composable
 fun AboutScreenPreview() {
     MyPDAMTheme {
-        AboutScreen()
+        AboutScreen(rememberNavController())
     }
 }
